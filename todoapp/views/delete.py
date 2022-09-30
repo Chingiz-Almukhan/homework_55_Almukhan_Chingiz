7751f76aeb279f27d57247baf4836e0b0574da30
Almukhan Chingiz <chingizpain@gmail.com>
2022-09-30 10:54:10 +0600

    Добавил методы для множественного удаления

--- todoapp/views/delete.py
+++ todoapp/views/delete.py
@@ -7,3 +7,20 @@ def delete(request, pk):
     articles = Todo.objects.get(pk=pk)
     articles.delete()
     return redirect('main')
+
+
+def delete_view(request):
+    articles = Todo.objects.all()
+    context = {
+        'articles': articles,
+        'statuses': [('new', 'Новая'), ('in process', 'В процессе'),
+                     ('ready', 'Сделано')],
+    }
+    return render(request, 'delete_tasks.html', context)
+
+
+def multi_delete(request):
+    task_id = request.POST.getlist('delete')
+    for pk in task_id:
+        Todo.objects.get(pk=pk).delete()
+    return redirect('main')
